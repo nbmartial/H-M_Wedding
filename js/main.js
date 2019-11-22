8fa8b40df4c0d2a906da3fef6507aaf5fa52b869
nbmartial <nbmartial@yahoo.fr>
2019-11-22 12:07:05 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -327,14 +327,6 @@
 		
 
 });
-
- $("input#username").on({  keydown: function(e) {
- if (e.which === 32)
- return false;
-},
-change: function() {
-this.value = this.value.replace(/\s/g, "");
-}
 			 
 }
  
